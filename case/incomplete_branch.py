b3214fea3567b9f5e64705462b6d71a6d1408c91
perinze <perinzefaper@foxmail.com>
2024-03-25 15:24:57 +0800

    reduce requirement for if-expr

--- case/incomplete_branch.py
+++ case/incomplete_branch.py
@@ -2,6 +2,19 @@ def branch(s, a, b):
     t1 = 1
     if s == 0:
         t1 = a + b
-        t2 = 5
     t3 = t1 * b
     return t3
+
+# 2   t1.1 = literal(1)
+# 
+# 3   t2.2 = literal(UNDEFINED)
+# 4   t1.3 = add(a, b)
+# 5   t2.4 = literal(5)
+# 
+# 3   literal.5 = literal(0)
+#     if.6 = eq(s, literal.5)
+#     t1.7 = sel(if.6, [t1.1, t1.3])
+#     t2.8 = sel(if.6, [t2.2, t2.4])
+# 
+# 6   t3.9 = mul(t1.7, b)
+# 7   ret t3.10 = identity(t3.9)
--- case/nested_branch.py
+++ case/nested_branch.py
@@ -2,6 +2,8 @@ def branch(s, a, b):
     r = 0
     if s == 0:
         r = a + b
+    elif s == 1:
+        r = a - b
     else:
         r = a * b
     return r
